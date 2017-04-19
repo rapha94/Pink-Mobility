a47fcb7232253f8c7fa4b8f5169f3776074cdeca
rapha94 <raphael.presberg@gmail.com>
2017-04-19 20:03:47 +0200

    ac débuts de bluetooth

--- app/src/main/java/com/example/pinkmobility/MainActivity.java
+++ app/src/main/java/com/example/pinkmobility/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.pinkmobility;
 
+import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final static int REQUEST_CODE_ENABLE_BLUETOOTH = 0;
     public static Button b_entrer;
 
     @Override
@@ -15,18 +18,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         go_tableauDB();
+        testBluetooth();
 
     }
 
 
-
-    public void go_tableauDB(){
+    public void go_tableauDB() {
 
         b_entrer = (Button) findViewById(R.id.Start);
-
-        //b_entrer.setEnabled(false);
-        //b_entrer.setBackgroundColor(808080);
-
         b_entrer.setEnabled(true);
         b_entrer.setOnClickListener(
                 new View.OnClickListener() {
@@ -42,6 +41,16 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void testBluetooth() {
 
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
+        if (!bluetoothAdapter.isEnabled()) {
+
+            b_entrer.setEnabled(false);
+            b_entrer.setBackgroundColor(808080);
+            Intent enableBlueTooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBlueTooth, REQUEST_CODE_ENABLE_BLUETOOTH);
+        }
+    }
 }
